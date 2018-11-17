8d0409ec4de3a18d47cc16d3cdabfe67af44449e
Matheus Roberto <matheus456456@gmail.com>
2018-11-17 16:59:56 -0200

    Consertando erro para verificar funcionamento do circleci

--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import './PageLayout.scss'
 
 export const PageLayout = ({ children }) => (
+  <div className='container text-center'>
     <h1>React Redux Starter Kit</h1>
     <IndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</IndexLink>
     {' · '}
@@ -11,6 +12,7 @@ export const PageLayout = ({ children }) => (
     <div className='page-layout__viewport'>
       {children}
     </div>
+  </div>
 )
 PageLayout.propTypes = {
   children: PropTypes.node,
